ce3ee72569316eb4bb498165b28b9723a5484b76
nunahsan sapnawi <40780080+nunahsan@users.noreply.github.com>
2024-01-13 00:13:13 +0800

    Update Routes.php

--- myproject/app/Routes.php
+++ myproject/app/Routes.php
@@ -17,9 +17,12 @@ if (getenv('SERVERNAME') == 'routing') {
     $routes = [
         '/v1/user/login' => [
             'module' => 'module1'
+        ],
+        '/v1/wallet/deposit' => [
+            'module' => 'module2'
         ]
     ];
-} else {
+} else if (getenv('SERVERNAME') == 'module1') {
     //let say this is module1
     $routes = [
         '/v1/user/login' => [
@@ -33,6 +36,21 @@ if (getenv('SERVERNAME') == 'routing') {
                     'minLength' => 3,
                     'maxLength' => 5,
                     'exactLength' => 4
+                ]
+            ]
+        ],
+    ];
+} else {
+    //let say this is module2
+    $routes = [
+        '/v1/wallet/deposit' => [
+            'namespace' => 'Api\V1\User',
+            'class' => 'Wallet',
+            'action' => 'deposit',
+            'params' => [
+                'amount' => [
+                    'type' => Rules::Double,
+                    'required' => Rules::Required
                 ]
             ]
         ],
