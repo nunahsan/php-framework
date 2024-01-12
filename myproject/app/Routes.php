2db617efd9b71a763238c81930886a9cc6638804
nunahsan sapnawi <40780080+nunahsan@users.noreply.github.com>
2024-01-12 23:42:58 +0800

    Update Routes.php

--- myproject/app/Routes.php
+++ myproject/app/Routes.php
@@ -10,35 +10,32 @@ class Rules
     const Optional = false;
 };
 
-const ROUTES = [
-    '/v1/user/login' => [
-        'namespace' => 'Api\V1\User',
-        'class' => 'Auth',
-        'action' => 'login',
-        'params' => [
-            'username' => [
-                'type' => Rules::String,
-                'required' => Rules::Required,
-                'minLength' => 3,
-                'maxLength' => 5,
-                'exactLength' => 4
-            ]
-        ]
-    ],
-    '/v1/user/list' => [
-        'namespace' => 'Api\V1\User',
-        'class' => 'Auth',
-        'action' => 'listUser'
-    ],
-    '/v2/user/login' => [
-        'namespace' => 'V2\User',
-        'class' => 'Auth',
-        'action' => 'login'
-    ],
-    '/v1/user/test' => [
-        'namespace' => 'Api\V1\User',
-        'class' => 'Auth',
-        'action' => 'test'
-    ],
-];
 
+
+if (getenv('SERVERNAME') == 'routing') {
+    //let say this is module routing
+    $routes = [
+        '/v1/user/login' => [
+            'module' => 'module1'
+        ]
+    ];
+} else {
+    //let say this is module1
+    $routes = [
+        '/v1/user/login' => [
+            'namespace' => 'Api\V1\User',
+            'class' => 'Auth',
+            'action' => 'login',
+            'params' => [
+                'username' => [
+                    'type' => Rules::String,
+                    'required' => Rules::Required,
+                    'minLength' => 3,
+                    'maxLength' => 5,
+                    'exactLength' => 4
+                ]
+            ]
+        ],
+    ];
+}
+define('ROUTES', $routes);
